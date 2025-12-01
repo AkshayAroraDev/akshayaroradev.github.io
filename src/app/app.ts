92c360d40237862c8f3a1032e14c05a84f43c525
Akshay Arora <akshay10103401@gmail.com>
2025-12-01 17:57:27 +0530

    used tracing-beam from ngxui to add scroll bar on the left

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,14 +2,13 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CustomTimelineComponent } from './custom-timeline/custom-timeline.component';
 import { AppHeroComponent } from './components/app-hero/app-hero.component';
-import { TracingBeamDirective } from './directives/tracing-beam.directive';
+import { NgxTracingBeamComponent } from '@omnedia/ngx-tracing-beam';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CustomTimelineComponent, AppHeroComponent],
+  imports: [RouterOutlet, CustomTimelineComponent, AppHeroComponent, NgxTracingBeamComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss',
-  hostDirectives: [TracingBeamDirective]
+  styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('portfolio-app');
