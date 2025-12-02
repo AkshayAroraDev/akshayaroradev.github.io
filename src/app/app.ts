b133645a96e9e50ccbc80ecaa74209cb0a2736d8
Akshay Arora <akshay10103401@gmail.com>
2025-12-02 18:39:58 +0530

    fixing build and console errors

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,11 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import { CustomTimelineComponent } from './custom-timeline/custom-timeline.component';
 import { AppHeroComponent } from './components/app-hero/app-hero.component';
 import { AppSkillsComponent } from './components/app-skills/app-skills.component';
-import { NgxTracingBeamComponent } from '@omnedia/ngx-tracing-beam';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CustomTimelineComponent, AppHeroComponent, AppSkillsComponent, NgxTracingBeamComponent],
+  imports: [RouterOutlet, CustomTimelineComponent, AppHeroComponent, AppSkillsComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
